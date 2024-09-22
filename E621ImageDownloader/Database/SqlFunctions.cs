af543121f6decff382eb69ec43e775208077ccad
Raven Ebonscale <CreateHavoc@hotmail.com>
2024-09-22 15:47:36 -0400

    Add project files.

---
+++ E621ImageDownloader/Database/SqlFunctions.cs
@@ -0,0 +1,92 @@
+﻿
+
+using Microsoft.Data.Sqlite;
+
+namespace E621ImageDownloader.Database
+{
+    internal class SqlFunctions
+    {
+
+        private readonly string _connectionoath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\E621db.sqlite";
+
+      
+     
+        /// <summary>
+        /// Creates The Database and Fills it out with Data~
+        /// </summary>
+        public void CreateDatabase()
+        {
+            if (File.Exists(_connectionoath) == false)
+            {
+
+                File.Create(_connectionoath);
+                SqliteConnection connection = new($"Data Source={_connectionoath}");
+
+
+                connection.Open();
+                var x = connection.State;
+
+                string sql = "Create Table tags (tag varchar(200));";
+
+                SqliteCommand cmd = new SqliteCommand(sql, connection);
+
+
+                cmd.ExecuteNonQuery();
+
+                sql = "Create Table ids (tag varchar(200) ,id int);";
+
+                connection.Open();
+                cmd = new SqliteCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+                connection.Dispose();
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Read The Sql Database
+        /// </summary>
+        /// <returns>A list of all tags added to the database</returns>
+        public async Task<List<string>> Read()
+        {
+            List<string> list = new List<string>();
+            SqliteConnection connection = new($"Data Source={_connectionoath}");
+
+            await connection.OpenAsync();
+
+            string sql = "Select tag  From tags";
+
+            SqliteCommand cmd = new SqliteCommand(sql,connection);
+
+
+
+            using (var reader = await cmd.ExecuteReaderAsync()) {
+                while (reader.Read()) {
+
+
+                    string name = reader.GetString(0);
+
+                    
+                    
+                
+                }
+            }
+
+           
+            return list;
+
+
+          
+
+        }
+
+
+
+    }
+    
+}
---
+++ E621ImageDownloader/Program.cs
@@ -0,0 +1,29 @@
+﻿using E621_Wrapper;
+using E621ImageDownloader.Database;
+
+namespace E621ImageDownloader
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Api x = new Api("", "", "Image Downloader v22.1");
+
+            string PictureFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            SqlFunctions sqlFunctions = new SqlFunctions();
+
+            sqlFunctions.CreateDatabase();
+
+            List<string> tags = await sqlFunctions.Read();
+            
+                         
+            
+
+                
+            
+            
+        }
+    }
+}
